271170d70c4bd966cd3d2849cfcf0a4ad14e6f4f
Noam Shani <Noams156@gmail.com>
2021-11-30 17:25:15 +0100

    iterarion 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import contactsData from "./contacts.json";
 import './App.css';
 
 function App() {
+ 
+
+  const setInitialValue = () => {
+    return contactsData.slice(0,5)
+}
+  const [contacts, setContacts] = useState(() => setInitialValue())
+
+ const contactList = contacts.map(contact => {
   return (
+    <tr key={contact.id}>
+  <td> <img src={contact.pictureUrl} style={{height: '50px'}} alt='contactPic' /> </td>
+      <td>{ contact.name }</td>
+      <td>{ contact.popularity }</td>
+      <td>{ contact.wonOscar ? <p>Oscar winner 🤩</p> : <p>no award</p>} </td>
+      <td>{ contact.wonOscar ? <p>Emmy winner 🤩</p> : <p> no award </p>} </td>
+    
+    </tr>
+  );
+})
+
+  return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <h1> IronContacts</h1>
+      <table>
+  <thead>
+    <tr>
+      <th> picture </th>
+      <th> name</th>
+      <th> popularity </th>
+      <th> Won Oscar </th>
+      <th> Won Emmy </th>
+      
+ 
+    </tr>
+  </thead>
+  <tbody>
+    {contactList}
+  </tbody>
+</table>
+   </div>
   );
 }
 
